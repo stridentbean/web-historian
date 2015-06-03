5ecb4d032e18c280f9bc76f142feea9a5b0947a4
stridentbean <borglin.me@gmail.com>
2015-06-03 15:34:18 -0700

    First get is working

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,39 @@
 var path = require('path');
+var dispatcher = require('httpdispatcher');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+var sendMessage = function(req, res){
+  res.writeHead(200, httpHelpers.headers);
+  res.end();
 };
+
+exports.handleRequest = function (req, res) {
+  try {
+    console.log('Serving request type ' + req.method + ' for url ' + req.url);
+    if(req.method === 'OPTIONS'){
+      //TODO: define this
+      sendMessage(req, res);
+    }else{
+      dispatcher.dispatch(req, res);
+    }
+  }catch(err){
+    console.log(err);
+  }
+  // res.end(archive.paths.list);
+};
+
+dispatcher.onGet("/", function(req, res) {
+  res.writeHead(200, httpHelpers.headers);
+
+  fs.readFile(path.join(__dirname, './public/index.html'), function(err, data){
+    if(err){
+      console.log(err);
+    }else{
+      res.end(data);
+    }
+  });
+
+});
